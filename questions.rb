5d9baa189ecbe6cc400916a53a91fb33a28d32f3
Alex Thon <ghaiquol@gmail.com>
2022-01-04 22:07:25 -0500

    Add main.rb and questions.rb files

---
+++ questions.rb
@@ -0,0 +1,14 @@
+# generates questions
+# values generated range from 1 to 20 => rand(1..20)
+
+class Question 
+  attr_accessor :answer
+  def initialize()
+    @num_one = rand(1..20)
+    @num_two = rand(1..20)
+    puts "What does #{@num_one} plus #{@num_two} equals?"
+    @answer = @num_one + @num_two
+  end
+end
+
+q = Question.new
